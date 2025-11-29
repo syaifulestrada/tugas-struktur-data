a8c32065aa140034a3ef874748fdc1a74ff605c3
syaifulestrada <syaifultheminister@gmail.com>
2025-11-29 14:13:50 +0700

    Done mengerjakan tugas 3 bfs

---
+++ tugas-3/bfs/Main.java
@@ -0,0 +1,117 @@
+import java.util.*;
+
+class Node {
+    String label;
+    int value;
+
+    Node(String label, int value) {
+        this.label = label;
+        this.value = value;
+    }
+}
+
+class Graph {
+
+    private Map<String, List<String>> adj = new HashMap<>();
+    private Map<String, Node> nodes = new HashMap<>();
+
+    public void addNode(String label, int value) {
+        nodes.put(label, new Node(label, value));
+        adj.put(label, new ArrayList<>());
+    }
+
+    public void addEdge(String a, String b) {
+        adj.get(a).add(b);
+        adj.get(b).add(a);
+    }
+
+    public List<String> bfs(String start, int target) {
+
+        Queue<String> q = new LinkedList<>();
+        Map<String, String> parent = new HashMap<>();
+        Set<String> visited = new HashSet<>();
+
+        q.add(start);
+        visited.add(start);
+        parent.put(start, null);
+
+        while (!q.isEmpty()) {
+            String cur = q.poll();
+            Node n = nodes.get(cur);
+
+            if(n.value == target) {
+                return buildPath(parent, cur);
+            }
+
+            for(String neighbor : adj.get(cur)) {
+                if(visited.add(neighbor)) {
+                    parent.put(neighbor, cur);
+                    q.add(neighbor);
+                }
+            }
+
+        }
+
+        return null;
+    }
+
+    private List<String> buildPath(Map<String, String> parent, String end) {
+
+        List<String> path = new ArrayList<>();
+
+        while(end != null) {
+            path.add(end);
+            end = parent.get(end);
+        }
+
+        Collections.reverse(path);
+        return path;
+    }
+
+
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph g = new Graph();
+
+        String[] labels = {"a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8"};
+        int[] values = {10, 7, 5, 12, 3, 13, 7, 20};
+
+        for(int i = 0; i < labels.length; i++) {
+            g.addNode(labels[i], values[i]);
+        }
+
+        g.addEdge("a1", "a2");
+        g.addEdge("a1", "a3");
+        g.addEdge("a2", "a4");
+        g.addEdge("a2", "a5");
+        g.addEdge("a3", "a6");
+        g.addEdge("a5", "a6");
+        g.addEdge("a4", "a7");
+        g.addEdge("a6", "a8");
+
+        System.out.println("Temukan nilai 13");
+        System.out.println(g.bfs("a1", 13));
+
+
+    }
+
+}
+
+/*
+
+            a1(10)
+            |    \
+          a2(7)  a3(5)
+            |   \    \
+        a4(12) a5(3)  a6(13)
+           |            \
+         a7(7)          a8(20) 
+
+*/
+
